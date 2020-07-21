cde325bebfe24d978e8bba6cf913dba798c858e5
ごま <gomayumax@users.noreply.github.com>
2020-07-21 22:31:57 +0900

    add getProducts()

--- index.js
+++ index.js
@@ -34,4 +34,17 @@ function getUsers() {
     });
 }
 
-getUsers();
+function getProducts() {
+  // https://github.com/firebase/snippets-node/blob/d769695bd1159103e7c877849ccaccab3db37039/firestore/main/index.js#L41
+  db.collection('products').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+      });
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
+}
+
+getProducts();
